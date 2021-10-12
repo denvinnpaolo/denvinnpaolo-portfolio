e38f44d500762a097606d13bbb66565f3d52207f
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-10-12 14:58:14 -0700

    adding containers and lists

---
+++ src/Screens/Home/FeaturedContianer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SectionContainer } from '../../Shared/Styles/Containers';
+
+const FeaturedContiner = (props) => {
+    return(
+        <SectionContainer background="white">
+            <div>Featured Work</div>
+        </SectionContainer>
+    );
+};
+
+export default FeaturedContiner;
